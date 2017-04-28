44f7c93a6cbf32465d1eae153869b29ce8bb72d0
Colton Burdick <elbo_macoroni@hotmail.com>
2017-04-28 02:44:34 -0400

    Add files via upload
    Uploaded Awesomacro source code.

---
+++ Macro/Macro/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Macro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.textResp = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Reload Responses";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Made by Colton/Saltonara";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "in Visual Studio 2010 (C#)";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Press INSERT to have Awesomacro pick a random response and type it in-game.";
+            this.notifyIcon1.BalloonTipTitle = "Awesomacro";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Awesomacro";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 26);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Uses InputSimulator made by\r\n      michaelnoonan";
+            // 
+            // textResp
+            // 
+            this.textResp.AutoSize = true;
+            this.textResp.ForeColor = System.Drawing.Color.ForestGreen;
+            this.textResp.Location = new System.Drawing.Point(7, 9);
+            this.textResp.Name = "textResp";
+            this.textResp.Size = new System.Drawing.Size(0, 13);
+            this.textResp.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::Macro.Properties.Resources.Awesomacro_New;
+            this.pictureBox1.Image = global::Macro.Properties.Resources.Awesomacro_New;
+            this.pictureBox1.InitialImage = global::Macro.Properties.Resources.Awesomacro_New;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 125);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(157, 192);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textResp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Awesomacro";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label textResp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
+
---
+++ Macro/Macro/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using WindowsInput;
+using System.Threading;
+
+namespace Macro
+{
+    public partial class Form1 : Form
+    {
+        // List.
+        List<string> respList = new List<string>();
+        List<string> usedList = new List<string>();
+
+        // Randomizer.
+        Random ranGen = new Random();
+        public int ranGet = -1;
+
+        // Constructor.
+        public Form1()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = false;
+            LoadResponses();
+            HotKeyManager.RegisterHotKey(Keys.Insert, KeyModifiers.NoRepeat);
+            HotKeyManager.HotKeyPressed += new EventHandler<HotKeyEventArgs>(HitMacro);
+        }
+
+        // Reload button.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            respList.Clear();
+            LoadResponses();
+        }
+
+        // Notification tray icon.
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Hide();
+            this.WindowState = FormWindowState.Minimized;
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        // Loading responses.
+        private void LoadResponses()
+        {
+            // File doesn't exist.
+            if (!File.Exists(@"Responses.txt"))
+            {
+                MessageBox.Show("Responses.txt could not be found.", "Awesomacro Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // File exists.
+            else
+            {
+                System.Collections.Generic.IEnumerable<String> tempRead = File.ReadLines(@"Responses.txt");
+                for (int i = 0; i < tempRead.Count(); i++)
+                {
+                    if (tempRead.ElementAt(i).Length > 0) respList.Add(tempRead.ElementAt(i));
+                }
+                if (respList.Count == 0) MessageBox.Show("Responses.txt is empty.", "Awesomacro Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.textResp.Text = respList.Count + " responses loaded.";
+                ranGet = -1;
+                usedList.Clear();
+            }
+        }
+
+        // Using the macro.
+        private void HitMacro(object sender, HotKeyEventArgs e)
+        {
+            if (respList.Count > 0) SimType();
+        }
+
+        // Simulating typing.
+        private void SimType()
+        {
+            // Getting response.
+            ranGet = ranGen.Next(0, respList.Count - 1);
+
+            // Letting go of directional keys.
+            InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_W);
+            InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_A);
+            InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_S);
+            InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_D);
+
+            // Opening chat.
+            InputSimulator.SimulateKeyDown(VirtualKeyCode.SHIFT);
+            InputSimulator.SimulateKeyDown(VirtualKeyCode.RETURN);
+            Thread.Sleep(10);
+            InputSimulator.SimulateKeyUp(VirtualKeyCode.SHIFT);
+            InputSimulator.SimulateKeyUp(VirtualKeyCode.RETURN);
+
+            // Typing.
+            Char[] tempChar = respList.ElementAt(ranGet).ToCharArray();
+            for (int i = 0; i < tempChar.Length; i++) {
+                TypeChar(tempChar[i]);
+                //if (i > 0 && (i / 25) == Math.Round(i / 25)) Thread.Sleep(10);
+            }
+
+            // Sending.
+            InputSimulator.SimulateKeyDown(VirtualKeyCode.RETURN);
+            Thread.Sleep(10);
+            InputSimulator.SimulateKeyUp(VirtualKeyCode.RETURN);
+
+            // List management.
+            if (respList.Count == 1 && usedList.Count > 0)
+            {
+                List<string> tempList = respList;
+                respList = usedList;
+                usedList = tempList;
+            }
+            else if (respList.Count > 1) {
+                usedList.Add(respList.ElementAt(ranGet));
+                respList.RemoveAt(ranGet);
+            }
+        }
+
+        // Getting character to type.
+        private void TypeChar(Char inChar) {
+            // Converting char to string.
+            String outStr = inChar.ToString();
+
+            // String not correctly handled by SendKeys.
+            if (outStr.Equals("(")) TypeShift(VirtualKeyCode.VK_9);
+            else if (outStr.Equals(")")) TypeShift(VirtualKeyCode.VK_0);
+            else if (outStr.Equals("^")) TypeShift(VirtualKeyCode.VK_6);
+            else if (outStr.Equals("%")) TypeShift(VirtualKeyCode.VK_5);
+            else if (outStr.Equals("{")) TypeShift(VirtualKeyCode.OEM_4);
+            else if (outStr.Equals("}")) TypeShift(VirtualKeyCode.OEM_6);
+            else if (outStr.Equals("~")) TypeShift(VirtualKeyCode.OEM_3);
+            else if (outStr.Equals("+")) TypeLittle(VirtualKeyCode.ADD);
+
+            // Characters (specified for quicker typing).
+            else if (outStr.Equals("A")) TypeShift(VirtualKeyCode.VK_A);
+            else if (outStr.Equals("B")) TypeShift(VirtualKeyCode.VK_B);
+            else if (outStr.Equals("C")) TypeShift(VirtualKeyCode.VK_C);
+            else if (outStr.Equals("D")) TypeShift(VirtualKeyCode.VK_D);
+            else if (outStr.Equals("E")) TypeShift(VirtualKeyCode.VK_E);
+            else if (outStr.Equals("F")) TypeShift(VirtualKeyCode.VK_F);
+            else if (outStr.Equals("G")) TypeShift(VirtualKeyCode.VK_G);
+            else if (outStr.Equals("H")) TypeShift(VirtualKeyCode.VK_H);
+            else if (outStr.Equals("I")) TypeShift(VirtualKeyCode.VK_I);
+            else if (outStr.Equals("J")) TypeShift(VirtualKeyCode.VK_J);
+            else if (outStr.Equals("K")) TypeShift(VirtualKeyCode.VK_K);
+            else if (outStr.Equals("L")) TypeShift(VirtualKeyCode.VK_L);
+            else if (outStr.Equals("M")) TypeShift(VirtualKeyCode.VK_M);
+            else if (outStr.Equals("N")) TypeShift(VirtualKeyCode.VK_N);
+            else if (outStr.Equals("O")) TypeShift(VirtualKeyCode.VK_O);
+            else if (outStr.Equals("P")) TypeShift(VirtualKeyCode.VK_P);
+            else if (outStr.Equals("Q")) TypeShift(VirtualKeyCode.VK_Q);
+            else if (outStr.Equals("R")) TypeShift(VirtualKeyCode.VK_R);
+            else if (outStr.Equals("S")) TypeShift(VirtualKeyCode.VK_S);
+            else if (outStr.Equals("T")) TypeShift(VirtualKeyCode.VK_T);
+            else if (outStr.Equals("U")) TypeShift(VirtualKeyCode.VK_U);
+            else if (outStr.Equals("V")) TypeShift(VirtualKeyCode.VK_V);
+            else if (outStr.Equals("W")) TypeShift(VirtualKeyCode.VK_W);
+            else if (outStr.Equals("X")) TypeShift(VirtualKeyCode.VK_X);
+            else if (outStr.Equals("Y")) TypeShift(VirtualKeyCode.VK_Y);
+            else if (outStr.Equals("Z")) TypeShift(VirtualKeyCode.VK_Z);
+            else if (outStr.Equals("a")) TypeLittle(VirtualKeyCode.VK_A);
+            else if (outStr.Equals("b")) TypeLittle(VirtualKeyCode.VK_B);
+            else if (outStr.Equals("c")) TypeLittle(VirtualKeyCode.VK_C);
+            else if (outStr.Equals("d")) TypeLittle(VirtualKeyCode.VK_D);
+            else if (outStr.Equals("e")) TypeLittle(VirtualKeyCode.VK_E);
+            else if (outStr.Equals("f")) TypeLittle(VirtualKeyCode.VK_F);
+            else if (outStr.Equals("g")) TypeLittle(VirtualKeyCode.VK_G);
+            else if (outStr.Equals("h")) TypeLittle(VirtualKeyCode.VK_H);
+            else if (outStr.Equals("i")) TypeLittle(VirtualKeyCode.VK_I);
+            else if (outStr.Equals("j")) TypeLittle(VirtualKeyCode.VK_J);
+            else if (outStr.Equals("k")) TypeLittle(VirtualKeyCode.VK_K);
+            else if (outStr.Equals("l")) TypeLittle(VirtualKeyCode.VK_L);
+            else if (outStr.Equals("m")) TypeLittle(VirtualKeyCode.VK_M);
+            else if (outStr.Equals("n")) TypeLittle(VirtualKeyCode.VK_N);
+            else if (outStr.Equals("o")) TypeLittle(VirtualKeyCode.VK_O);
+            else if (outStr.Equals("p")) TypeLittle(VirtualKeyCode.VK_P);
+            else if (outStr.Equals("q")) TypeLittle(VirtualKeyCode.VK_Q);
+            else if (outStr.Equals("r")) TypeLittle(VirtualKeyCode.VK_R);
+            else if (outStr.Equals("s")) TypeLittle(VirtualKeyCode.VK_S);
+            else if (outStr.Equals("t")) TypeLittle(VirtualKeyCode.VK_T);
+            else if (outStr.Equals("u")) TypeLittle(VirtualKeyCode.VK_U);
+            else if (outStr.Equals("v")) TypeLittle(VirtualKeyCode.VK_V);
+            else if (outStr.Equals("w")) TypeLittle(VirtualKeyCode.VK_W);
+            else if (outStr.Equals("x")) TypeLittle(VirtualKeyCode.VK_X);
+            else if (outStr.Equals("y")) TypeLittle(VirtualKeyCode.VK_Y);
+            else if (outStr.Equals("z")) TypeLittle(VirtualKeyCode.VK_Z);
+            else if (outStr.Equals("0")) TypeLittle(VirtualKeyCode.VK_0);
+            else if (outStr.Equals("1")) TypeLittle(VirtualKeyCode.VK_1);
+            else if (outStr.Equals("2")) TypeLittle(VirtualKeyCode.VK_2);
+            else if (outStr.Equals("3")) TypeLittle(VirtualKeyCode.VK_3);
+            else if (outStr.Equals("4")) TypeLittle(VirtualKeyCode.VK_4);
+            else if (outStr.Equals("5")) TypeLittle(VirtualKeyCode.VK_5);
+            else if (outStr.Equals("6")) TypeLittle(VirtualKeyCode.VK_6);
+            else if (outStr.Equals("7")) TypeLittle(VirtualKeyCode.VK_7);
+            else if (outStr.Equals("8")) TypeLittle(VirtualKeyCode.VK_8);
+            else if (outStr.Equals("9")) TypeLittle(VirtualKeyCode.VK_9);
+            else if (outStr.Equals("!")) TypeShift(VirtualKeyCode.VK_1);
+            else if (outStr.Equals("@")) TypeShift(VirtualKeyCode.VK_2);
+            else if (outStr.Equals("#")) TypeShift(VirtualKeyCode.VK_3);
+            else if (outStr.Equals("$")) TypeShift(VirtualKeyCode.VK_4);
+            else if (outStr.Equals("&")) TypeShift(VirtualKeyCode.VK_7);
+            else if (outStr.Equals("*")) TypeShift(VirtualKeyCode.VK_8);
+
+            // Normally type it.
+            else SendKeys.Send(outStr);
+        }
+
+        // Shifted character.
+        private void TypeShift(VirtualKeyCode keyIn) {
+            InputSimulator.SimulateModifiedKeyStroke(VirtualKeyCode.LSHIFT, keyIn);
+        }
+
+        // Normal character.
+        private void TypeLittle(VirtualKeyCode keyIn) {
+            InputSimulator.SimulateKeyPress(keyIn);
+        }
+
+        // End.
+    }
+}
